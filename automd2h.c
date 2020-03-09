be3a1c64ff2dda5eef8778b85f6e120426a697d8
Trafalgar <masyyaf@gmail.com>
2020-03-08 21:38:56 -0400

    up

--- automd2h.c
+++ automd2h.c
@@ -19,10 +19,9 @@
 #include <sys/inotify.h>
 #include <signal.h>
 
-#define EVENT_SIZE  ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 #define BUF_LEN (1024 * (EVENT_SIZE + 16))
-
 
 #define USAGE "\n\
 Usage: [-h|--help] [-t| ] [-n| ] [-w| ] [-r| ] [-f| ]\n\
@@ -688,76 +687,83 @@ void Delete_Child(pid_t c_pid_To_Delete, int sec)
     //killing the child after a certain delay.
     kill(c_pid_To_Delete, SIGKILL);
 }
-#define EVENT_SIZE  ( sizeof (struct inotify_event) )
-#define EVENT_BUF_LEN     ( 1024 * ( EVENT_SIZE + 16 ) )
+#define EVENT_SIZE (sizeof(struct inotify_event))
+#define EVENT_BUF_LEN (1024 * (EVENT_SIZE + 16))
 // Listen in the current directories
 int watch(char *Dir)
 {
-  int length, i = 0;
-  int fd;
-  int wd;
-  char buffer[EVENT_BUF_LEN];
-
-  /*creating the INOTIFY instance*/
-  fd = inotify_init();
-
-  /*checking for error*/
-  if ( fd < 0 ) {
-    perror( "inotify_init" );
-  }
-
-  /*adding the “/tmp” directory into watch list. Here, the suggestion is to validate the existence of the directory before adding into monitoring list.*/
-  wd = inotify_add_watch( fd, Dir, IN_CREATE | IN_DELETE | IN_MODIFY );
-
-  /*read to determine the event change happens on “/tmp” directory. Actually this read blocks until the change event occurs*/ 
 
     while (true)
     {
-        length = read( fd, buffer, EVENT_BUF_LEN ); 
+        int length, i = 0;
+        int fd;
+        int wd;
+        char buffer[EVENT_BUF_LEN];
+
+        /*creating the INOTIFY instance*/
+        fd = inotify_init();
 
         /*checking for error*/
-        if ( length < 0 ) {
-            perror( "read" );
-        }  
+        if (fd < 0)
+        {
+            perror("inotify_init");
+        }
+
+        /*adding the “/tmp” directory into watch list. Here, the suggestion is to validate the existence of the directory before adding into monitoring list.*/
+        wd = inotify_add_watch(fd, Dir, IN_CREATE | IN_DELETE | IN_MODIFY);
+
+        /*read to determine the event change happens on “/tmp” directory. Actually this read blocks until the change event occurs*/
+        length = read(fd, buffer, EVENT_BUF_LEN);
+
+        /*checking for error*/
+        if (length < 0)
+        {
+            perror("read");
+        }
 
         /*actually read return the list of change events happens. Here, read the change event one by one and process it accordingly.*/
-        while ( i < length ) {     
-            struct inotify_event *event = ( struct inotify_event * ) &buffer[ i ];     
-            if ( event->len ) {
-            if ( event->mask & IN_CREATE ) {
-                if ( event->mask & IN_ISDIR ) {
-                printf( "New directory %s created.\n", event->name );
+        while (i < length)
+        {
+            struct inotify_event *event = (struct inotify_event *)&buffer[i];
+            if (event->len)
+            {
+                if (event->mask & IN_CREATE)
+                {
+                    if (event->mask & IN_ISDIR)
+                    {
+                        printf("New directory %s created.\n", event->name);
+                    }
+                    else
+                    {
+                        printf("New file %s created.\n", event->name);
+                    }
                 }
-                else {
-                printf( "New file %s created.\n", event->name );
+                else if (event->mask & IN_DELETE)
+                {
+                    if (event->mask & IN_ISDIR)
+                    {
+                        printf("Directory %s deleted.\n", event->name);
+                    }
+                    else
+                    {
+                        printf("File %s deleted.\n", event->name);
+                    }
                 }
-            }
-            else if ( event->mask & IN_DELETE ) {
-                if ( event->mask & IN_ISDIR ) {
-                printf( "Directory %s deleted.\n", event->name );
+                else if (event->mask & IN_MODIFY)
+                {
+                    printf("File %s deleted.\n", event->name);
                 }
-                else {
-                printf( "File %s deleted.\n", event->name );
-                }
-            }
-            else if( event->mask & IN_MODIFY ) {
-            printf( "File %s deleted.\n", event->name );
-            }
             }
             i += EVENT_SIZE + event->len;
         }
         /*removing the “/tmp” directory from the watch list.*/
-        inotify_rm_watch( fd, wd );
-
+        inotify_rm_watch(fd, wd);
         /*closing the INOTIFY instance*/
-        close( fd );    
-   }
-    
+        close(fd);
+    }
 
-   return 0;
-
+    return 0;
 }
-
 
 int watch2(char *file)
 {
